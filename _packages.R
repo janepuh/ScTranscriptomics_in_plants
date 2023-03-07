3f2c75c2418e40c8d0defe4357db4be78f0e0bc9
VictoriaVMironova <victoria.mironova@ru.nl>
2023-03-07 17:25:35 +0100

    The file with necessary packages created

---
+++ _packages.R
@@ -0,0 +1,21 @@
+# Install packages
+
+my_packages <- c("tidyverse", 
+                 "ggplot2", 
+                 "Seurat", 
+                 "DoubletDecon", 
+                 "xaringan", 
+                 "xaringanExtra", 
+                 "devtools")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+install.packages(my_packages)
+
+devtools::install_github('cole-trapnell-lab/monocle3')
+
+devtools::install_github('renderthis')
+install.packages('pdftools')
+
+
