a5f89136d9c56c7bc3244844f346b8ad780184de
VictoriaVMironova <victoria.mironova@ru.nl>
2023-03-09 17:49:38 +0100

    dittoseq added

--- _packages.R
+++ _packages.R
@@ -10,12 +10,11 @@ my_packages <- c("tidyverse",
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
+BiocManager::install("dittoSeq")
 
 install.packages(my_packages)
 
 devtools::install_github('cole-trapnell-lab/monocle3')
-
 devtools::install_github('renderthis')
 install.packages('pdftools')
-
 
