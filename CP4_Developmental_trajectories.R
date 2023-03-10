8078d06179450604033ee97316564104d8999360
VictoriaVMironova <victoria.mironova@ru.nl>
2023-03-10 16:58:13 +0100

    CP4 files created

---
+++ CP4_Developmental_trajectories.R
@@ -0,0 +1,15 @@
+#The script for computer practice 4:
+#Developmental trajectories, developmental states
+
+#Step 0. Preparatory. Loading libraries and the dataset.
+#Use _packages.R to install all the necessary packages prior loading the libraries
+
+library(Seurat)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(monocle3)
+set.seed(100)
+
+leaf.dataset<-readRDS('Data/leaf.dataset.rds')
+
