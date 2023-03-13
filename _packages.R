7cc182609d4ccde1acd6d56898c9902615ba744b
VictoriaVMironova <victoria.mironova@ru.nl>
2023-03-13 13:38:37 +0100

    monocle 3 package install updated

--- _packages.R
+++ _packages.R
@@ -12,9 +12,16 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("dittoSeq")
 
+#install the dependencies for Monocle3
+BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
+                       'limma', 'lme4', 'S4Vectors', 'SingleCellExperiment',
+                       'SummarizedExperiment', 'batchelor', 'HDF5Array',
+                       'terra', 'ggrastr'))
+
 install.packages(my_packages)
 
 devtools::install_github('cole-trapnell-lab/monocle3')
 devtools::install_github('renderthis')
+devtools::install_github('cole-trapnell-lab/monocle3', ref="develop")
 install.packages('pdftools')
 
