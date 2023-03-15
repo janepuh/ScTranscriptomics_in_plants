27c9b7a49b3e997a15dcaa132d5ed06e43249289
VictoriaVMironova <victoria.mironova@ru.nl>
2023-03-15 09:55:25 +0100

    monocle dependencies updated

--- _packages.R
+++ _packages.R
@@ -8,6 +8,8 @@ my_packages <- c("tidyverse",
                  "xaringanExtra", 
                  "devtools")
 
+install.packages(my_packages)
+
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("dittoSeq")
@@ -17,11 +19,15 @@ BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
                        'limma', 'lme4', 'S4Vectors', 'SingleCellExperiment',
                        'SummarizedExperiment', 'batchelor', 'HDF5Array',
                        'terra', 'ggrastr'))
+#install the dependencies for Seurat Wrapper
+my_packages_2<- c("R.utils", "spatstat.explore", "spatstat.random")
+install.packages(my_packages2)
 
-install.packages(my_packages)
-
+devtools::install_github('satijalab/seurat-data')
+devtools::install_github("satijalab/seurat-wrappers")
 devtools::install_github('cole-trapnell-lab/monocle3')
+
+
 devtools::install_github('renderthis')
-devtools::install_github('cole-trapnell-lab/monocle3', ref="develop")
 install.packages('pdftools')
 
