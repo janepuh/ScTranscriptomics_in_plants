d961de7342d90fa8fca41bf1e7ff51ed3ff8ddf2
VictoriaVMironova <victoria.mironova@ru.nl>
2023-03-17 13:33:25 +0100

    Splitted packages in basci and extra parts

---
+++ Extra/_packages_extra.R
@@ -0,0 +1,20 @@
+#Install extra_packages
+
+#install the dependencies for Monocle3
+BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
+                       'limma', 'lme4', 'S4Vectors', 'SingleCellExperiment',
+                       'SummarizedExperiment', 'batchelor', 'HDF5Array',
+                       'terra', 'ggrastr'))
+devtools::install_github('cole-trapnell-lab/monocle3')
+
+#install the dependencies for Seurat Wrapper
+my_packages_2<- c("R.utils", "spatstat.explore", "spatstat.random")
+install.packages(my_packages2)
+
+devtools::install_github('satijalab/seurat-data')
+devtools::install_github("satijalab/seurat-wrappers")
+
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("dittoSeq")
--- _packages.R
+++ _packages.R
@@ -1,33 +1,11 @@
 # Install packages
 
 my_packages <- c("tidyverse", 
-                 "ggplot2", 
+                 "sp", 
+                 "Matrix", 
+                 "SeuratObject", 
                  "Seurat", 
-                 "DoubletDecon", 
                  "xaringan", 
                  "xaringanExtra", 
                  "devtools")
-
 install.packages(my_packages)
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("dittoSeq")
-
-#install the dependencies for Monocle3
-BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
-                       'limma', 'lme4', 'S4Vectors', 'SingleCellExperiment',
-                       'SummarizedExperiment', 'batchelor', 'HDF5Array',
-                       'terra', 'ggrastr'))
-#install the dependencies for Seurat Wrapper
-my_packages_2<- c("R.utils", "spatstat.explore", "spatstat.random")
-install.packages(my_packages2)
-
-devtools::install_github('satijalab/seurat-data')
-devtools::install_github("satijalab/seurat-wrappers")
-devtools::install_github('cole-trapnell-lab/monocle3')
-
-
-devtools::install_github('renderthis')
-install.packages('pdftools')
-
